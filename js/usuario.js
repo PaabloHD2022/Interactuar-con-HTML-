b9373e0fb776df7b71e59b17b1691a0c096ce47d
Pablo Demartini <106122869+PaabloHD2022@users.noreply.github.com>
2022-08-10 23:14:33 -0300

    Nuevos archivos...
    Este trabajo es nuevo porque eliminé lo que tenia. No me aparecia funcionando en la pagina de github.

---
+++ js/usuario.js
@@ -0,0 +1,11 @@
+class Usuario {
+
+    constructor(nombre, diaNac, mesNac, anioNac,){
+
+        this.nombre = nombre;
+        this.diaNac = diaNac;
+        this.mesNac = mesNac;
+        this.anioNac = anioNac;
+    }
+}
+
